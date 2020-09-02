1f1e11edf613c7305b6a9dde8f11b6cacd587188
Ethan Holen <ethanholen@gmail.com>
2020-09-01 18:34:19 -0600

    added snappable grid for one shape and non functional buttons

---
+++ Assets/ItemSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject item;
+    public Transform spawnPoint;
+
+
+    public void Spawn()
+    {
+        GameObject spawnedBullet = Instantiate(item, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
