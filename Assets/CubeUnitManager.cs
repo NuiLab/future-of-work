7ce439cb2b2864d524aa8a97608dd1864e17cb6d
Ethan Holen <ethanholen@gmail.com>
2020-09-15 12:52:27 -0600

    good testing environment setup on large cube testing

---
+++ Assets/CubeUnitManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeUnitManager : MonoBehaviour
+{
+
+    public GameObject attatchedItem;
+
+
+    private List<Collider> colliders = new List<Collider>();
+    public List<Collider> GetColliders () { return colliders; }
+
+ 
+
+     private void OnTriggerEnter (Collider other) {
+         if (!colliders.Contains(other)) { colliders.Add(other); }
+     }
+
+     private void OnTriggerExit (Collider other) {
+         colliders.Remove(other);
+         if(other.gameObject.tag == "block")
+        {
+            Destroy(other.gameObject);
+        }
+     }
+}
