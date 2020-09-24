8e100926d824cac38873f14dca2a018952d8f8b1
Ethan Holen <ethanholen@gmail.com>
2020-09-24 12:38:37 -0600

    Custom Socket scene created blank

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public GameObject[] grid_blocks;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //foreach (GameObject block in grid_blocks)
+        //{
+        //    Debug.Log("Hi im " + block.name);
+
+        //    block.SetActive(true);
+
+
+
+        //    // https://docs.unity3d.com/ScriptReference/Collider.OnTriggerStay.html
+
+        //}
+
+    }
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+        
+    //}
+}
