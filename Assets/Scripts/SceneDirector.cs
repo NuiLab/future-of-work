a43f48aaf3bd47246f88d68ab9d86abe6abd3423
EthanHolen <ethanholen@gmail.com>
2021-03-24 12:19:59 -0600

    scene transitioning added and demo2

---
+++ Assets/Scripts/SceneDirector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneDirector : MonoBehaviour
+{
+
+    public void OpenMenu()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 0);
+
+    }
+     
+    public void OpenShapeA()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+
+    }
+
+    public void OpenShapeC()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 2);
+
+    }
+
+}
