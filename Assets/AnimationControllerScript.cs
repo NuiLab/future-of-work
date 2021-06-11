8d52a9c43fee2031391a7d14c387b9a03b055489
Ethan Holen <ethanholen@gmail.com>
2021-06-11 10:58:41 -0600

    Better folder structure for scenes

---
+++ Assets/AnimationControllerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControllerScript : MonoBehaviour
+{
+
+
+    public GameObject barA;
+    public GameObject barB;
+    public float speed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Make barA move towards barB
+
+        if(barA.transform.position != barB.transform.position)
+        {
+
+            
+           
+        }
+        
+    }
+}
